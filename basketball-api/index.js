d15c6543d1c60c4d5187bb7c7fd5866c5973ac42
Andre Luige <Andre.Luige@nortal.com>
2023-01-19 17:32:26 +0200

    Adding swagger

--- basketball-api/index.js
+++ basketball-api/index.js
@@ -1,6 +1,10 @@
 const app = require("express")()
 const port = 8080
 
+const swaggerUI = require("swagger-ui-express")
+const swaggerDocument = require("./swagger.json")
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })
