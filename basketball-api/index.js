a9f15f8fc8b9ecece6e8c58094672b125d888efc
Andre Luige <Andre.Luige@nortal.com>
2023-01-26 14:06:56 +0200

    Added database connection, updated env.example and testing connections with 'customers' table

--- basketball-api/index.js
+++ basketball-api/index.js
@@ -1,9 +1,34 @@
 require("dotenv").config()
 
 const app = require("express")()
+const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 const swaggerUI = require("swagger-ui-express")
 const swaggerDocument = require("./swagger.json")
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    connectionLimit: 5
+})
+
+app.get("/customers", async (req, res) => {
+    let connection 
+    try {
+        connection = await pool.getConnection()
+        const rows = await connection.query("SELECT * FROM customers")
+        console.log(rows)
+        res.send(rows)
+    } catch (error) {
+        throw error
+    } finally {
+        if (connection) {
+            return connection.end()
+        }
+    }
+})
 
 app.get("/games", (req, res) => {
     res.send(["Paide Cup", "Veteranide Loivamine"])
