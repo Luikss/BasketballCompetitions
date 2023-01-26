283de3569b68654828a8f5142317ab281a58738c
Andre Luige <Andre.Luige@nortal.com>
2023-01-26 13:31:54 +0200

    Added get API-s and updated documentation

--- basketball-api/index.js
+++ basketball-api/index.js
@@ -2,9 +2,21 @@ require("dotenv").config()
 
 const app = require("express")()
 const port = process.env.APP_PORT
-
 const swaggerUI = require("swagger-ui-express")
 const swaggerDocument = require("./swagger.json")
+
+app.get("/games", (req, res) => {
+    res.send(["Paide Cup", "Veteranide Loivamine"])
+})
+
+app.get("/teams", (req, res) => {
+    res.send(["Tõrva Sopsutajad", "Saaremaa Dunkers"])
+})
+
+app.get("/players", (req, res) => {
+    res.send(["Andre Luige", "Dagne Markiine Kotkas"])
+})
+
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.listen(port, () => {
