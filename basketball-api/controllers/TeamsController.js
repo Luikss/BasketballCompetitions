b5d83ca5397822a23c9a83628d032fd66de56036
markiine <dmkotkas@gmail.com>
2023-02-02 19:28:54 +0200

    Add code handle errors [#184268345]

--- basketball-api/controllers/TeamsController.js
+++ basketball-api/controllers/TeamsController.js
@@ -15,6 +15,28 @@ exports.getById = async (req, res) => {
     res.send(teams)
 }
 exports.createNew = async (req, res) => {
-    const teams = await Team.createNew(req.body)
-    res.send(teams)
+    let team
+  try {
+     team = await Team.create(req.body)
+   } catch (error) {
+     if (error instanceof db.Sequelize.ValidationError) {
+       res.status(400).send({"error":"Invalid input"})
+       res.status(400).send({"error":error.errors.map((item)=> item.message)})
+     } else {
+       res.status(500).send({"error":"Something went wrong on our side. Sorry :("})
+    }
+    return
+  }
+  res
+    .status(201)
+    .location(`${getBaseUrl(req)}/teams/${team.id}`)
+    .json(team)
 }
+getBaseUrl = (request) => {
+  return (
+    (request.connection && request.connection.encrypted ? "https" : "http") +
+    `://${request.headers.host}`
+  )
+}
+
+
