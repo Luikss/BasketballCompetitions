59f2a85c588728090c2d56622b8c1f52e622ac70
markiine <dmkotkas@gmail.com>
2023-02-02 14:15:02 +0200

    Add error 404 to endpoint #184268126

--- basketball-api/controllers/TeamsController.js
+++ basketball-api/controllers/TeamsController.js
@@ -8,5 +8,9 @@ exports.getAll = async (req, res) => {
 
 exports.getById = async (req, res) => {
     const teams = await Team.findByPk(req.params.id)
+    if(teams === null) {
+        res.status(404).send({"error": "Team not found"})
+        return
+    }
     res.send(teams)
 }
