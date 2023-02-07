a09871b2bd3aa5a51f6412fd3660c47b29f9aa34
Andre Luige <Andre.Luige@nortal.com>
2023-02-07 12:22:51 +0200

    Reformating code [#184268345]

--- basketball-api/controllers/TeamsController.js
+++ basketball-api/controllers/TeamsController.js
@@ -2,28 +2,28 @@ const { db } = require("../db")
 const Team = db.teams
 
 exports.getAll = async (req, res) => {
-    const teams = await Team.findAll({attributes:["name"]})
-    res.send(teams)
+  const teams = await Team.findAll({attributes:["name"]})
+  res.send(teams)
 }
 
 exports.getById = async (req, res) => {
-    const teams = await Team.findByPk(req.params.id)
-    if(teams === null) {
-        res.status(404).send({"error": "Team not found"})
-        return
-    }
-    res.send(teams)
+  const teams = await Team.findByPk(req.params.id)
+  if (teams === null) {
+    res.status(404).send({"error": "Team not found"})
+      return
+  }
+  res.send(teams)
 }
+
 exports.createNew = async (req, res) => {
-    let team
+  let team
   try {
-     team = await Team.create(req.body)
-   } catch (error) {
-     if (error instanceof db.Sequelize.ValidationError) {
-       res.status(400).send({"error":"Invalid input"})
-       res.status(400).send({"error":error.errors.map((item)=> item.message)})
-     } else {
-       res.status(500).send({"error":"Something went wrong on our side. Sorry :("})
+    team = await Team.create(req.body)
+  } catch (error) {
+    if (error instanceof db.Sequelize.ValidationError) {
+      res.status(400).send({"error":error.errors.map((item)=> item.message)})
+    } else {
+      res.status(500).send({"error":"Something went wrong on our side. Sorry :("})
     }
     return
   }
@@ -38,5 +38,3 @@ getBaseUrl = (request) => {
     `://${request.headers.host}`
   )
 }
-
-
