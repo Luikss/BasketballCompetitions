7e980b7926eb682aa8fec92dd12168f6c1d3bcc5
markiine <dmkotkas@gmail.com>
2023-02-09 16:05:09 +0200

    Delete game [Delivers #184268371]

--- basketball-api/controllers/GamesController.js
+++ basketball-api/controllers/GamesController.js
@@ -9,7 +9,7 @@ exports.getAll = async (req, res) => {
 exports.getById = async (req, res) => {
     const games = await Game.findByPk(req.params.id)
     if(games === null) {
-        res.status(404).send({"error": "Game not found"})
+        res.status(404).send({error: "Game not found"})
         return
     }
     res.send(games)
@@ -21,9 +21,9 @@ exports.createNew = async (req, res) => {
       game = await Game.create(req.body)
     } catch (error) {
       if (error instanceof db.Sequelize.ValidationError) {
-        res.status(400).send({"error":error.errors.map((item)=> item.message)})
+        res.status(400).send({error:error.errors.map((item)=> item.message)})
       } else {
-        res.status(500).send({"error":"Something went wrong on our side. Sorry :("})
+        res.status(500).send({error:"Something went wrong on our side. Sorry :("})
       }
       return
     }
@@ -32,10 +32,26 @@ exports.createNew = async (req, res) => {
       .location(`${getBaseUrl(req)}/games/${game.id}`)
       .json(game)
   }
+
+exports.deleteById = async (req, res) => {
+    let result
+    try {
+        result = await Game.destroy({where: {id: req.params.id}})
+    } catch (error) {
+        res.status(500).send({error:"Something went wrong on our side. Sorry :("})
+        return
+    }
+    if (result === 0) {
+        res.status(404).send({error:"Game not found"})
+        return
+    }
+    res.status(204).send()
+}
+
   
-  getBaseUrl = (request) => {
+getBaseUrl = (request) => {
     return (
-      (request.connection && request.connection.encrypted ? "https" : "http") +
-      `://${request.headers.host}`
+        (request.connection && request.connection.encrypted ? "https" : "http") +
+        `://${request.headers.host}`
     )
-  }
+}
