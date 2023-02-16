563bfdc4e17390056c48afbbb524462c62fe7988
Andre Luige <Andre.Luige@nortal.com>
2023-02-16 15:15:19 +0200

    Working on game and team relation (in progress) #184455833

--- basketball-api/controllers/GamesController.js
+++ basketball-api/controllers/GamesController.js
@@ -5,23 +5,40 @@ exports.getAll = async (req, res) => {
   const games = await Game.findAll({
     include: { all: true }
   })
-  console.log(games);
+
   let result = []
   result = games.map( (g) => {
     return {
-        "gameName":g.dataValues.name,
+        "name": g.dataValues.name,
     }
-  }) 
-  res.send(games)
+  })
+  res.send(result)
 }
 
 exports.getById = async (req, res) => {
-  const games = await Game.findByPk(req.params.id)
-  if (games === null) {
+  const game = await Game.findByPk(req.params.id, {
+    include: { all: true }
+  })
+
+  if (game === null) {
       res.status(404).send({error: "Game not found"})
       return
   }
-  res.send(games)
+
+  let games = [game]
+  let result = []
+  result = games.map( (g) => {
+    return {
+        "id": g.dataValues.id,
+        "name": g.dataValues.name,
+        "location": g.dataValues.location,
+        "teamOneName": g.dataValues.teamOne.dataValues.name,
+        "teamTwoName": g.dataValues.teamTwo.dataValues.name,
+        "teamOneScore": g.dataValues.teamOneScore,
+        "teamTwoScore": g.dataValues.teamTwoScore
+    }
+  })
+  res.send(result)
 }
 
 exports.createNew = async (req, res) => {
