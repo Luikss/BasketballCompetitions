a9a4b8144d229ed6f5decd852c5f5877f03cb264
markiine <dmkotkas@gmail.com>
2023-03-02 13:27:39 +0200

    Testing frontend message

--- basketball-front-vue3/src/App.vue
+++ basketball-front-vue3/src/App.vue
@@ -1,23 +1,17 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hello World"
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    {{ msg }}
+  </div>
 </template>
 
 <style scoped>
